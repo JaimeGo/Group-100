f945b9f4a3bedf8b6d973f5c25f67f785edc1142
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-09-14 00:32:09 -0300

    getting users

--- entrega_2/src/routes/users.js
+++ entrega_2/src/routes/users.js
@@ -2,4 +2,10 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.get('users', '/', async (ctx) => {
+  const users = await ctx.orm.User.findAll();
+  await ctx.render('users/index', {users});
+}
+  )
+
 module.exports = router;
