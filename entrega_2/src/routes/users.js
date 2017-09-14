74dc9689f676d3923e2d4aef08d95a1a5c2161fb
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-09-14 09:07:18 -0300

    creating users

--- entrega_2/src/routes/users.js
+++ entrega_2/src/routes/users.js
@@ -5,7 +5,19 @@ const router = new KoaRouter();
 router.get('users', '/', async (ctx) => {
   const users = await ctx.orm.User.findAll();
   await ctx.render('users/index', {users});
-}
-  )
+})
+
+router.get('newUser', '/new', async (ctx) => {
+  const user = await ctx.orm.User.build();
+  await ctx.render('users/new', {
+    user,
+    createUserPath: ctx.router.url('createUser'),
+  });
+})
+
+router.post('createUser', '/', async (ctx) => {
+  const user = await ctx.orm.User.create(ctx.request.body);
+  ctx.redirect(ctx.router.url('users'));
+})
 
 module.exports = router;
