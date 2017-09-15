e850d60032d4b55809dfc420c0d5df5f64e426ec
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-09-15 19:26:44 -0300

    basic of questions router

--- entrega_2/src/routes/questions.js
+++ entrega_2/src/routes/questions.js
@@ -4,11 +4,54 @@ const router = new KoaRouter();
 
 router.get('questions', '/', async (ctx) => {
   const {user} = ctx.state;
-  // const questions = ctx.orm.question.find({
-  //   where: { userId: user.id },
-  // })
   const questions = await user.getQuestions();
   await ctx.render('questions/index', {user, questions});
+})
+
+router.get('newQuestion', '/new', async (ctx) => {
+	const {user} = ctx.state;
+	const question = await ctx.orm.user.build();
+	await ctx.render('questions/new', {
+		user, 
+		question,
+		createQuestionPath: ctx.router.url('createQuestion');
+	});
+})
+
+router.post('createQuestion', '/:id', async (ctx) => {
+	const {user} = ctx.state;
+	const question = await user.createQuestion(ctx.params.id);
+	ctx.redirect(ctx.routes.url('questions'));
+})
+
+router.get('editQuestion', '/:id/edit', async (ctx) => {
+	const {user} = ctx.state;
+	const question = await ctx.orm.question.findById(ctx.params.id);
+	await ctx.render('questions/edit', {
+		user, 
+		question,
+		updateQuestionPath: ctx.router.url('updateQuestion');
+	});
+})
+
+router.patch('updateQuestion', '/:id', async (ctx) => {
+	const {user} = ctx.state;
+	const question = await ctx.orm.question.findById(ctx.params.id);
+	ctx.redirect(ctx.routes.url('questions'));
+})
+
+router.delete('deleteQuestion', '/:id', async (ctx) => {
+  const {user} = ctx.state;
+  await user.destroyQuestion({
+    where: { id: ctx.params.id },
+  });
+  ctx.redirect(ctx.router.url('questions')); 
+})
+
+router.get('question', '/:id', async (ctx) => {
+	const {user} = ctx.state;
+	const question = user.findById(params.ctx.id);
+	render('questions/show', {user, question});
 })
 
 module.exports = router;
