412f39f05438124f30dd62e11655e8550f4ed3e3
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-10-05 23:07:24 -0300

    admin in seeds

--- entrega_2/src/seeds/20170913013509-add-users.js
+++ entrega_2/src/seeds/20170913013509-add-users.js
@@ -4,9 +4,12 @@ module.exports = {
   up(queryInterface) {
     const usersData = [];
     for (let i = 0; i < 20; i += 1) {
+      admin_ = false;
+      if (i == 0) { admin_ = true };
       usersData.push({
         name: faker.company.companyName(),
         password: faker.lorem.words(),
+        admin: admin_,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
