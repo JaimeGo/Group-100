5e9ba965d521f67bc224998fca34b3e5d0527731
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-10-28 23:16:49 -0300

    landing page funcionando (copied, not merged)

--- Grupo-100/src/routes/index.js
+++ Grupo-100/src/routes/index.js
@@ -5,9 +5,10 @@ const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
 	const exams= await ctx.orm.exam.findAll();
+	const questions= await ctx.orm.exam.findAll();
 	
 
- 	 await ctx.render('index', { appVersion: pkg.version, exams });
+ 	 await ctx.render('index', { appVersion: pkg.version, exams, questions });
 });
 
 module.exports = router;
