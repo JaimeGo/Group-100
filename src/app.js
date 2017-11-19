be4fd6aeaecf851ca5554e16a3a29c78d5d71576
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-11-18 22:17:39 -0300

    added some api references, but no using it yet

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 const ui = require('./ui-app');
-
+const api = require('./api-app')
 // module.exports = { ui };
 module.exports = ui;
---
+++ src/routes/api/index.js
@@ -0,0 +1,23 @@
+const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
+// const ongsRoutes = require('./ongs');
+// const authRoutes = require('./auth');
+
+const router = new KoaRouter();
+
+// unauthenticated endpoints
+// router.use('/auth', authRoutes.routes());
+
+// JWT authentication without passthrough (error if not authenticated)
+router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+router.use(async (ctx, next) => {
+  if (ctx.state.authData.userId) {
+    ctx.state.currentUser = await ctx.orm.user.findById(ctx.state.authData.userId);
+  }
+  return next();
+});
+
+// authenticated endpoints
+// router.use('/ongs', ongsRoutes.routes());
+
+module.exports = router;
