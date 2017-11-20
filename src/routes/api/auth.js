43c1586f23e4b4bd6f568ea135d21717e11861fd
AlfonsoIgnacioV <aivaldes@uc.cl>
2017-11-20 11:15:21 -0300

    some proofs (not functional)

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -5,7 +5,22 @@ const router = new KoaRouter();
 
 router.post('auth', '/', async (ctx) => {
   console.log("Authentificating!! ")
-  console.log("Body: ", ctx.body)
+  console.log("ctx.request.body: ", ctx.request.body)
+  const { email, password } = ctx.request.body;
+  const user = await ctx.orm.user.find({ where: { email } });
+  if (user && await user.checkPassword(password)) {
+    const token = await new Promise((resolve, reject) => {
+      jwtgenerator.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET,
+        (err, tokenResult) => (err ? reject(err) : resolve(tokenResult)),
+      );
+    });
+    console.log("after promise of token")
+    ctx.body = { token };
+  } else {
+    ctx.throw(401, 'Wrong e-mail or password');
+  }
 });
 
 module.exports = router;
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -8,8 +8,9 @@ const router = new KoaRouter();
 // unauthenticated endpoints
 router.use('/auth', authRoutes.routes());
 
+console.log("after use auth")
 // JWT authentication without passthrough (error if not authenticated)
-// router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
 // router.use(async (ctx, next) => {
 //   if (ctx.state.authData.userId) {
 //     ctx.state.currentUser = await ctx.orm.user.findById(ctx.state.authData.userId);
